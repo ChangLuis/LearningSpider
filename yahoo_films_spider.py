32a0e1fda2436c7b05429dc2bdcecc0d92a83807
ChangLuis <somebodyforluis@gmail.com>
2019-06-04 02:00:48 +0800

    Modified the time'pattern from regular string to the string of module of datetime.

--- yahoo_films_spider.py
+++ yahoo_films_spider.py
@@ -3,6 +3,7 @@
 
 import requests
 from bs4 import BeautifulSoup as BS
+from datetime import datetime
 
 res = requests.get("https://movies.yahoo.com.tw/movie_comingsoon.html?page=1")
 res = BS(res.text,"lxml")
@@ -14,7 +15,9 @@ for film in films:
     film_eng_title = titles[1].text.strip()
     film_address = titles[0]["href"]
 
-    time = film.select(".release_movie_time")[0].text.lstrip("上映日期 ： ")
-    print(f"{film_chinese_title}, {film_eng_title}, {time}")
+    time = film.select(".release_movie_time")[0].text
+    dt = datetime.strptime(time,"上映日期 ： %Y-%m-%d")
+    dt_formate = dt.strftime("%Y-%m-%d")
+    print(f"{film_chinese_title}, {film_eng_title}, {dt_formate}")
     print(film_address,"\n")
 
