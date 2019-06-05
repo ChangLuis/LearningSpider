e473669829528aacee5a6937a07066dea8fb5fd3
ChangLuis <somebodyforluis@gmail.com>
2019-06-05 23:48:31 +0800

    Packing the film's detail into function.

--- yahoo_films_spider.py
+++ yahoo_films_spider.py
@@ -5,23 +5,21 @@ import requests
 from bs4 import BeautifulSoup as BS
 from datetime import datetime
 
-res = requests.get("https://movies.yahoo.com.tw/movie_comingsoon.html?page=1")
-res = BS(res.text,"lxml")
-films = res.select(".release_list .release_info")
+def get_page_films():
+    res = requests.get("https://movies.yahoo.com.tw/movie_comingsoon.html?page=1")
+    res = BS(res.text,"lxml")
+    films = res.select(".release_list .release_info")
+    list_films = []
+    for film in films:
+        dict_film = {}
+        titles = film.select(".release_movie_name a")
+        dict_film["film_chinese_title"] = titles[0].text.strip()
+        dict_film["film_eng_title"] = titles[1].text.strip()
+        dict_film["film_address"] = titles[0]["href"]
 
-for film in films:
-    titles = film.select(".release_movie_name a")
-    film_chinese_title = titles[0].text.strip()
-    film_eng_title = titles[1].text.strip()
-    film_address = titles[0]["href"]
-
-    time = film.select(".release_movie_time")[0].text
-    dt = datetime.strptime(time,"上映日期 ： %Y-%m-%d")
-    dt_formate = dt.strftime("%Y-%m-%d")
-
-    content = film.select(".release_text span")[0].text.strip()
-
-    print(f"{film_chinese_title}, {film_eng_title}, {dt_formate}")
-    print(film_address,"\n")
-    print(content,"\n")
-
+        time = film.select(".release_movie_time")[0].text
+        dt = datetime.strptime(time,"上映日期 ： %Y-%m-%d")
+        dict_film["dt_formate"] = dt.strftime("%Y-%m-%d")
+        dict_film["content"] = film.select(".release_text span")[0].text.strip()
+        list_films.append(dict_film)
+    print(list_films)
